2c0654ce5bf94fa4278ff5d8260e7890b2dc0f5f
Emilvinther <95082486+Emilvinther@users.noreply.github.com>
2023-04-18 15:48:17 +0200

    Add files via upload

---
+++ Synqopg2-3/Synqopg2-3/Program.cs
@@ -0,0 +1,86 @@
+﻿public class Program
+{
+
+    // Making new lock object
+    static object _lock = new object();
+    // Counter that count how many chars are made
+    static int counter = 0;
+    static void Main(string[] args)
+    {
+
+
+        Thread stars = new Thread(Stjerner);
+        Thread hashtags = new Thread(Havelåger);
+        stars.Start();
+        hashtags.Start();
+
+
+
+
+    }
+
+
+    // Method that makes the star char, and runs the loop that writes the star char in a monitor lock
+    public static void Stjerner()
+    {
+        char star = '*';
+
+        while (true)
+        {
+
+            Monitor.Enter(_lock);
+            try
+            {
+                for (int i = 0; i < 60; i++)
+                {
+                    Console.Write(star);
+                    counter++;
+                    
+                }
+                Console.Write(counter);
+                Console.WriteLine("\n");
+                Thread.Sleep(500);
+            }
+            finally
+            {
+
+                Monitor.Exit(_lock);
+            }
+
+        }
+
+        
+    }
+
+    // Method that makes the hashtag char, and runs the loop that writes the hashtag char in a monitor lock
+    public static void Havelåger()
+    {
+        char hashtag = '#';
+
+        while (true)
+        {
+
+            Monitor.Enter(_lock);
+            try
+            {
+                for (int i = 0; i < 60; i++)
+                {
+                    Console.Write(hashtag);
+                    counter++;
+                    
+                }
+                Console.Write(counter);
+                Console.WriteLine("\n");
+                Thread.Sleep(500);
+            }
+            finally
+            {
+
+                Monitor.Exit(_lock);
+            }
+
+        }
+    }
+
+    
+}
---
+++ synqqopg1/synqqopg1/Program.cs
@@ -0,0 +1,72 @@
+﻿public class Program
+{
+    // number being counted
+    static int numb = 5;
+
+    // Lock opject
+    static object _lock = new object();
+
+    static void Main(String[] args)
+    {
+
+        // Makes threads
+        Thread countup = new Thread(UpCount);
+        Thread countdown = new Thread(DownCount);
+
+        // Starts threads
+        countup.Start();
+        countdown.Start();
+        
+
+
+
+        
+
+    }
+
+    // Method that counts up 2 with a monitor lock
+    public static void UpCount()
+    {
+        while (true)
+        {
+            Monitor.Enter(_lock);
+
+            try
+            {
+                numb += 2;
+                Console.WriteLine(numb);
+                Thread.Sleep(500);
+            }
+            finally
+            {
+                Console.WriteLine("locked");
+                Monitor.Exit(_lock);
+                
+            }
+        }
+    }
+
+
+    // Method that counts down 1 with a monitor lock
+    public static void DownCount()
+    {
+        while (true)
+        {
+            Monitor.Enter(_lock);
+            
+            try
+            {
+                numb -= 1;
+                Console.WriteLine(numb);
+                Thread.Sleep(500);
+            }
+            finally
+            { 
+            
+                Console.WriteLine("locked");
+                Monitor.Exit(_lock);
+                
+            }
+        }
+    }
+}
